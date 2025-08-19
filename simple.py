aeb69f8448c57420a7da3550d8a792004932128f
sutt <wsutton17@gmail.com>
2025-08-19 11:36:35 -0400

    basecamp for encode/decode

--- simple.py
+++ simple.py
@@ -27,12 +27,25 @@ SECRET_MESSAGE = bytes([13, 17])
 
 N_TRIALS = 20
 N_CHOICES = 10
+A_TOTAL = 1.1
+B_TOTAL = 1.3
 
 selections = []
 for _ in range(N_TRIALS):
-    choices = {i: random.random() for i in range(N_CHOICES)}
+    vals = [random.random() for _ in range(N_CHOICES)]
+    vals = sorted(vals, reverse=True)
+    choices = {i:v for i,v in enumerate(vals)}
     exps = [math.exp(v) for v in choices.values()]
     sum_exps = sum(exps)
+    sum_exps = sum_exps * random.uniform(A_TOTAL, B_TOTAL)
     selections.append({k: v / sum_exps for k, v in zip(choices.keys(), exps)})
 
-print(json.dumps(selections, indent=2))
+# check that all values in each dict sum to around 0.7 - 0.9
+# check = [sum(e.values()) for e in selections]
+# print(json.dumps(check, indent=2))
+
+# TODO - encode the secret message by sampling the selections items 
+# to produce a sequence of chars which represent the secret message
+# based on the idea that selections is static and will be known at
+# encode and decode time
+
