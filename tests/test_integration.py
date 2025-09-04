f57aea1efb8a86c7eb973a7bbc2097fffb50b4d1
sutt <wsutton17@gmail.com>
2025-09-04 17:33:59 -0400

    test: refactor previous test, add decoder-only integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,13 +4,16 @@ import pytest
 
 from stego_llm.core import main_decode, main_encode
 
-# Get model path from environment variable
-LLAMA_MODEL_PATH = os.environ.get("LLAMA_MODEL_PATH")
+# Get model path from environment variable: preferred and fallabck
+TEST_LLM_PATH = os.environ.get("INNOCUOUS_TEST_LLM_PATH")
+if TEST_LLM_PATH is None:
+    TEST_LLM_PATH = os.environ.get("INNOCUOUS_LLM_PATH")
 
 
 @pytest.mark.slow
 @pytest.mark.skipif(
-    not LLAMA_MODEL_PATH, reason="LLAMA_MODEL_PATH environment variable not set"
+    not TEST_LLM_PATH,
+    reason="Neither INNOCUOUS_TEST_LLM_PATH nor INNOCUOUS_LLM_PATH environment variable are set",
 )
 def test_encode_decode_integration():
     """
@@ -26,7 +29,7 @@ def test_encode_decode_integration():
         initial_prompt=initial_prompt,
         msg=message_to_encode,
         chunk_size=chunk_size,
-        llm_path=LLAMA_MODEL_PATH,
+        llm_path=TEST_LLM_PATH,
     )
 
     assert encoded_text is not None
@@ -37,8 +40,87 @@ def test_encode_decode_integration():
         encoded_prompt=encoded_text,
         initial_prompt=initial_prompt,
         chunk_size=chunk_size,
-        llm_path=LLAMA_MODEL_PATH,
+        llm_path=TEST_LLM_PATH,
     )
 
     assert decoded_message is not None
     assert decoded_message == message_to_encode
+
+
+@pytest.mark.slow
+@pytest.mark.skipif(
+    not TEST_LLM_PATH,
+    reason="Neither INNOCUOUS_TEST_LLM_PATH nor INNOCUOUS_LLM_PATH environment variable are set",
+)
+def test_decode_example_1():
+    """
+    Test decoder only with previously generated example
+    Will be used for punctuation re-insertion proof-of-concept
+    """
+    GENERATED_TEXT = """
+Below is an iambic penatameter poem. Complete it:
+The king of all the realms did lie there weak,
+With eyes that wept their last goodbye to day,
+The crown did glister by his lifeless cheeks,
+Yet still no heir to
+"""
+    GENERATED_TEXT = GENERATED_TEXT.strip()
+    assert GENERATED_TEXT == GENERATED_TEXT
+
+    encoded_message = b"hello world"
+    initial_prompt = "Below is an iambic penatameter poem. Complete it:\nThe king"
+
+    decoded_message = main_decode(
+        encoded_prompt=GENERATED_TEXT,
+        initial_prompt=initial_prompt,
+        chunk_size=3,
+        llm_path=TEST_LLM_PATH,
+    )
+
+    assert decoded_message == encoded_message
+
+
+@pytest.mark.xfail
+@pytest.mark.slow
+@pytest.mark.skipif(
+    not TEST_LLM_PATH,
+    reason="Neither INNOCUOUS_TEST_LLM_PATH nor INNOCUOUS_LLM_PATH environment variable are set",
+)
+def test_decode_example_2():
+    """
+    Test decoder only
+    Use example from previous test: test_decode_example_1
+    But with punctuation removed to confuse the decoding
+    """
+    GENERATED_TEXT_NO_PUNCTUATION = """
+Below is an iambic penatameter poem. Complete it:
+The king of all the realms did lie there weak
+With eyes that wept their last goodbye to day
+The crown did glister by his lifeless cheeks
+Yet still no heir to
+"""
+    GENERATED_TEXT_NO_PUNCTUATION = GENERATED_TEXT_NO_PUNCTUATION.strip()
+    assert GENERATED_TEXT_NO_PUNCTUATION == GENERATED_TEXT_NO_PUNCTUATION
+
+    encoded_message = b"hello world"
+    initial_prompt = "Below is an iambic penatameter poem. Complete it:\nThe king"
+
+    decoded_message = main_decode(
+        encoded_prompt=GENERATED_TEXT_NO_PUNCTUATION,
+        initial_prompt=initial_prompt,
+        chunk_size=3,
+        llm_path=TEST_LLM_PATH,
+    )
+
+    assert decoded_message == encoded_message
+
+
+if __name__ == "__main__":
+    import sys
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+
+    # test_encode_decode_integration()
+    # test_decode_example_1()
+    test_decode_example_2()
